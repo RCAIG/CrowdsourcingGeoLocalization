b3de03661b925224f82cb1d09a15a22cb7565bd0
hqb-joo <77607038+hqb-joo@users.noreply.github.com>
2025-10-10 12:56:11 +0800

    Update image folder paths and fix indentation

--- CrowdsourcingGeoLocalization/CrowdsourcingGeoLocalization/script/perspective.py
+++ CrowdsourcingGeoLocalization/CrowdsourcingGeoLocalization/script/perspective.py
@@ -3,9 +3,9 @@ import os
 import cv2
 import numpy as np
 
-image1_folder = 'D:\CrowdsourcingGeoLocalization\CrowdsourcingGeoLocalization\Data\ANU_data_small\polarmap_cvhk_all'
-image2_folder = 'D:\CrowdsourcingGeoLocalization\CrowdsourcingGeoLocalization\Data\ANU_data_small\streetview_cvhk_all'
-save_folder = 'D:\CrowdsourcingGeoLocalization\CrowdsourcingGeoLocalization\Data\ANU_data_small\polarmap_cvhk_all_caijian'
+image1_folder = 'D:\CrowdsourcingGeoLocalization\CrowdsourcingGeoLocalization\Data\ANU_data_small\polarmap_all'
+image2_folder = 'D:\CrowdsourcingGeoLocalization\CrowdsourcingGeoLocalization\Data\ANU_data_small\streetview_all'
+save_folder = 'D:\CrowdsourcingGeoLocalization\CrowdsourcingGeoLocalization\Data\ANU_data_small\polarmap_all_caijian'
 
 
 image1_files = [os.path.splitext(f)[0].replace('_satView', '') for f in os.listdir(image1_folder) if f.endswith('_satView.png')]
@@ -49,3 +49,4 @@ for file_name in image1_files:
     save_path = os.path.join(save_folder, file_name + '_satView_wrapped.png')
 
     cv2.imwrite(save_path, wrapped_pano_image)
+
